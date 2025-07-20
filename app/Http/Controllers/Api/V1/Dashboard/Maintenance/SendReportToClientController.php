5e5c4f94195306955bfc50b0100fc325c8ce7a52
MohamedHassan1996 <mohamedhassan00096@gmail.com>
2025-07-20 13:07:46 +0300

    t

--- app/Http/Controllers/Api/V1/Dashboard/Maintenance/SendReportToClientController.php
+++ app/Http/Controllers/Api/V1/Dashboard/Maintenance/SendReportToClientController.php
@@ -24,29 +24,29 @@ class SendReportToClientController extends Controller implements HasMiddleware
     }
 
     public function __invoke(Request $request)
-{
-    try {
-        DB::beginTransaction();
+    {
+        try {
+            DB::beginTransaction();
 
-        $maintenanceReports = MaintenanceReport::whereIn('maintenance_guid', $request->maintenanceGuids)->get();
+            $maintenanceReports = MaintenanceReport::whereIn('maintenance_guid', $request->maintenanceGuids)->get();
 
-        foreach ($maintenanceReports as $maintenanceReport) {
-            if(!empty($maintenanceReport->path)) {
-                 Mail::to('mr10dev10@gmail.com')->send(new SendMaintenanceReportMail(
-                $maintenanceReport->path
-                ));
+            foreach ($maintenanceReports as $maintenanceReport) {
+                if(!empty($maintenanceReport->path)) {
+                    Mail::to('mr10dev10@gmail.com')->send(new SendMaintenanceReportMail(
+                    $maintenanceReport->path
+                    ));
+                }
+
             }
 
+            DB::commit();
+
+            return ApiResponse::success([], 'Maintenance report sent successfully.');
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            throw $th;
         }
-
-        DB::commit();
-
-        return ApiResponse::success([], 'Maintenance report sent successfully.');
-    } catch (\Throwable $th) {
-        DB::rollBack();
-        throw $th;
     }
-}
 
 
 
